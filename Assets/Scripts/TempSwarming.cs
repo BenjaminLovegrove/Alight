01d0fdc5853d16312710d44f88d620c2ef091726
BenjaminLovegrove <Benjamin_jl@Live.com>
2015-04-20 17:37:58 +1000

    Added temp swarming and changed lighting

---
+++ Assets/Scripts/TempSwarming.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TempSwarming : MonoBehaviour {
+
+	float newposx;
+	float newposy;
+	Vector3 newpos;
+	public GameObject swarmPoint;
+	float changeDirTime;
+
+	//Editable variables
+	float swarmSpeed = 0.3f;
+	float swarmRange = 2.5f;
+	float swarmDirectionVolatility = 1f;
+	
+	void Start () {
+		//Set initial direction
+		ChangeDir ();
+
+		//Give fireflies varying timers
+		swarmDirectionVolatility = Random.Range (swarmDirectionVolatility * 0.5f, swarmDirectionVolatility * 1.5f);
+	}
+
+	void Update () {
+		//Move
+		transform.position = Vector3.MoveTowards (transform.position, newpos, swarmSpeed * Time.deltaTime);
+
+		//If it's been some time, change direction
+		changeDirTime -= Time.deltaTime;
+		if (changeDirTime <= 0) {
+			ChangeDir();
+		}
+	}
+	
+	void ChangeDir(){
+		//Set new location to head towards
+		newposx = Random.Range (swarmPoint.transform.position.x - swarmRange, swarmPoint.transform.position.x + swarmRange);
+		newposy = Random.Range (swarmPoint.transform.position.y - swarmRange, swarmPoint.transform.position.y + swarmRange);
+		newpos = new Vector3 (newposx, newposy, transform.position.z);
+
+		//Set countdown to next new direction
+		changeDirTime = swarmDirectionVolatility;
+	}
+
+}
