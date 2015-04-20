cdb6b550060cc28b7954f945db64feaa3b3efa9d
BenjaminLovegrove <Benjamin_jl@Live.com>
2015-04-20 17:59:03 +1000

    Added movement. Swarming changes.

---
+++ Assets/Scripts/Temp (testing)/TempMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TempMovement : MonoBehaviour {
+
+	Rigidbody rb;
+
+	void Start(){
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//transform.Translate (Input.GetAxis ("Horizontal") * Time.deltaTime, Input.GetAxis ("Vertical") * Time.deltaTime, transform.position.z);
+
+		rb.AddForce(new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime * 100, Input.GetAxis("Vertical") * Time.deltaTime * 100, 0));
+	}
+}
