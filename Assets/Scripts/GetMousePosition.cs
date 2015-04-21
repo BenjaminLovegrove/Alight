7919f9d09676c80ec3a3bba6abd88a9a6569f792
Stewman13 <110367@student.sae.edu.au>
2015-04-21 23:50:43 +1000

    Gets Mouse Position
    Gets Mouse Position, and moves mouse position prefab to said location.
    This is running in the lighting test scene

---
+++ Assets/Scripts/GetMousePosition.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GetMousePosition : MonoBehaviour {
+
+	private Vector3 targetPos;
+	
+	void Start() {
+		targetPos = transform.position;    
+	}
+	
+	void Update () {
+		float distance = transform.position.z - Camera.main.transform.position.z;
+		targetPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+		targetPos = Camera.main.ScreenToWorldPoint(targetPos);
+		
+		transform.position = Vector3.MoveTowards (transform.position, targetPos, 1);
+	}
+}
