91e7a5f90433e9938b0faabbfc970032cd81307d
Stewman13 <110367@student.sae.edu.au>
2015-04-22 00:30:29 +1000

    Swarm Movement Prt2
    should have been part of my previous commit

---
+++ Assets/Scripts/Temp (testing)/TESTMovementv3.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TESTMovementv3 : MonoBehaviour {
+
+	public Rigidbody rb;
+	public GameObject FireflyDragger;
+	Vector3 dir;
+	
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	void Update () {
+
+		Vector3 FireflyDraggerPos = FireflyDragger.transform.position;
+		dir = FireflyDraggerPos - transform.position;
+		
+		if (Input.GetMouseButton (0)) {
+			rb.AddForce (dir.normalized * Time.deltaTime * Random.Range(15,25));
+		}
+	}
+}
