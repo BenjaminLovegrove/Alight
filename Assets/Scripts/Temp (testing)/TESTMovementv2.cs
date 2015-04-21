cb30dcc898298fc6c79887aa3fed36b8a1e7c5c3
BenjaminLovegrove <Benjamin_jl@Live.com>
2015-04-21 23:19:31 +1000

    Finished level. Added mouse movement. Lighting.

---
+++ Assets/Scripts/Temp (testing)/TESTMovementv2.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TESTMovementv2 : MonoBehaviour {
+
+	public Rigidbody rb;
+	Vector3 worldMousePoint;
+	Vector3 dir;
+
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void Update () {
+		worldMousePoint = Camera.main.ScreenToWorldPoint (new Vector3(Input.mousePosition.x, Input.mousePosition.y, 30));
+		dir = worldMousePoint - transform.position;
+
+		if (Input.GetMouseButton (0)) {
+			rb.AddForce (dir.normalized * Time.deltaTime * 25);
+		}
+	}
+}
