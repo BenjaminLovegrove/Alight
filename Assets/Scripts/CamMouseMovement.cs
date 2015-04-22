8d4983624107e38a1707b74ef748d0ee24c0b33f
Benjamin <Benjamin_JL@live.com>
2015-04-22 13:23:59 +1000

    Mouse camera movement done

---
+++ Assets/Scripts/CamMouseMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamMouseMovement : MonoBehaviour {
+
+	Rigidbody rb;
+	Vector3 mousePos;
+	Vector3 screenCenterPoint;
+
+	void Start(){
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		mousePos = Camera.main.ScreenToWorldPoint(new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 30));
+		screenCenterPoint = Camera.main.ScreenToWorldPoint(new Vector3 (Screen.width/2, Screen.height/2, 30));
+
+		if (Vector3.Distance (mousePos, screenCenterPoint) > 10){
+			rb.AddForce((mousePos - screenCenterPoint) * Time.deltaTime * 2);
+		}
+	}
+}
