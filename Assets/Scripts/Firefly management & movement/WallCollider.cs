e715d9c1c144319c1c4b8077f1f0cf46107ea348
BenjaminLovegrove <Benjamin_jl@Live.com>
2015-04-23 01:10:39 +1000

    Firefly & wall collision

--- Assets/Scripts/Firefly management & movement/WallCollider.cs
+++ Assets/Scripts/Firefly management & movement/WallCollider.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class WallCollider : MonoBehaviour {
 
-	void OnCollisionEnter2D (Collision2D col) {
+	void OnCollisionEnter2D(Collision2D col) {
 		SendMessageUpwards ("WallCollide", SendMessageOptions.DontRequireReceiver);
 	}
 }
