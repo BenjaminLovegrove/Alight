c06a16db7656fedf1a1ed47621e1cca219d5a670
Stewman13 <110367@student.sae.edu.au>
2015-04-23 02:15:43 +1000

    Burnable Branch
    Added code to burn web and remove log.

--- Assets/Scripts/Interactables & Hazards/Web_Collide.cs
+++ Assets/Scripts/Interactables & Hazards/Web_Collide.cs
@@ -3,13 +3,36 @@ using System.Collections;
 
 public class Web_Collide : MonoBehaviour {
 
+	public GameObject Fire1;
+	public GameObject Fire2;
+	public GameObject Fire3;
+	public GameObject Log;
+
 	// Use this for initialization
 	void Start () {
-	
+		Fire1.SetActive(false);
+		Fire2.SetActive(false);
+		Fire3.SetActive(false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		if (collision.gameObject.tag == "FireFly"){
+			Destroy(collision.gameObject);
+			StartCoroutine(BurnMe(2.0F));
+		}
+	}
+	IEnumerator BurnMe(float waitTime) {
+		Fire1.SetActive(true);
+		Fire2.SetActive(true);
+		Fire3.SetActive(true);
+		yield return new WaitForSeconds(waitTime);
+		Log.rigidbody.isKinematic = false;
+		Log.rigidbody.useGravity = true;
+		Destroy(this.gameObject);
 	}
 }
