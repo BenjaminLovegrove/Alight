41e56bd0820f09aabb9b4b2b25386460d18d35df
Stewman13 <110367@student.sae.edu.au>
2015-04-23 00:54:48 +1000

    Added Particle Effects
    Added effects to folder Only!

---
+++ Assets/Scripts/Interactables & Hazards/Web_Collide.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Web_Collide : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
