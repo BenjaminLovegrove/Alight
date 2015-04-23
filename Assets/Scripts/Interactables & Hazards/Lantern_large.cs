fe2bfd1c0dda4ca2fd7b14e1df1c385c682c581d
BenjaminLovegrove <Benjamin_jl@Live.com>
2015-04-23 10:46:53 +1000

    Large lanters + checkpoints work.

--- Assets/Scripts/Interactables & Hazards/Lantern_large.cs
+++ Assets/Scripts/Interactables & Hazards/Lantern_large.cs
@@ -4,19 +4,28 @@ using System.Collections;
 public class Lantern_large : MonoBehaviour {
 	
 	public Light lanternLight; //To turn lantern on
+	public ParticleSystem particles;
 	AudioClip lanternEnable; //SFX
 	GameObject mainSwarm; //To send checkpoints to
 	public GameObject spawnPoint;
+
+	public bool startActive = false; // this is so the first lantern can start on.
 	
 	void Start () {
-		lanternLight = GetComponent<Light>();
+		lanternLight = GetComponentInChildren<Light>();
+		particles = GetComponentInChildren<ParticleSystem>();
 		mainSwarm = GameObject.FindGameObjectWithTag ("MainSwarm");
+
+		if (!startActive) {
+			particles.gameObject.SetActive (false);
+		}
 	}
 	
 	
-	void OnCollisionEnter (Collision col) {
-		if (col.gameObject.tag == "FireFly") {
+	void OnTriggerEnter (Collider col) {
+		if (col.gameObject.tag == "FireFly" && lanternLight.enabled == false) {
 			lanternLight.enabled = true;
+			particles.gameObject.SetActive(true);
 			//play sound.
 
 			mainSwarm.SendMessage ("Checkpoint", spawnPoint.transform.position);
