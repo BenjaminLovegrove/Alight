0023540594269862e681747a696fce59affa7fb3
BenjaminLovegrove <Benjamin_jl@Live.com>
2015-04-23 17:34:06 +1000

    Half way through final puzzle.

---
+++ Assets/Scripts/Final area & ending/FinalVine.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FinalVine : MonoBehaviour {
+
+	public GameObject[] lanterns;
+	int currentLantern = 1;
+	//Chime sound here
+	//Fail sound here
+	
+	void Start () {
+		lanterns = GameObject.FindGameObjectsWithTag ("finalLantern");
+	}
+
+	void Update(){
+		if (currentLantern >= 5) {
+			//Animate branch
+			print ("woo");
+		}
+	}
+
+	void LanternOn (int lanternNo) {
+		if (lanternNo == currentLantern) {
+			currentLantern ++;
+			//audio.pitch (lanternNo / 4);
+			//Play chime sound
+		} else {
+			foreach (GameObject lantern in lanterns){
+				lantern.SendMessage("Incorrect");
+				//Play incorrect sound;
+				currentLantern = 1;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Final area & ending/endLanterns.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class endLanterns : MonoBehaviour {
+
+	public int lanternNumber;
+	public GameObject endVine;
+	public Light lanternLight; //To turn lantern on
+	//public ParticleSystem particles;
+	
+	void Start () {
+		lanternLight = GetComponentInChildren<Light>();
+		endVine = GameObject.FindGameObjectWithTag ("finalVine");
+		//particles = GetComponentInChildren<ParticleSystem>();
+	}
+
+	void OnTriggerEnter (Collider col) {
+		if (col.gameObject.tag == "FireFly" && lanternLight.enabled == false) {
+			lanternLight.enabled = true;
+			endVine.SendMessage("LanternOn", lanternNumber);
+			//particles.gameObject.SetActive(true);
+		}
+	}
+
+	void Incorrect(){
+		Invoke ("TurnOff", 1f);
+	}
+
+	void TurnOff(){
+		lanternLight.enabled = false;
+	}
+}
