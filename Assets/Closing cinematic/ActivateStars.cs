e96f4d6368493015d00ab0b6963c083cac4427a6
BenjaminLovegrove <Benjamin_jl@Live.com>
2015-04-26 23:04:00 +1000

    Made closing cinematic better. Just needs dialogue

--- Assets/Closing cinematic/ActivateStars.cs
+++ Assets/Closing cinematic/ActivateStars.cs
@@ -3,13 +3,21 @@ using System.Collections;
 
 public class ActivateStars : MonoBehaviour {
 	
-	GameObject stars;
+	GameObject[] stars;
+	bool triggered = false;
 
 	void Start(){
-		stars = GameObject.FindGameObjectWithTag ("Star");
+		stars = GameObject.FindGameObjectsWithTag ("Star");
 	}
 
-	void OnCollisionEnter (Collision col) {
+	void OnTriggerEnter (Collider col) {
+		print (col.gameObject.tag);
+		if (triggered == false){
+			triggered = true;
 
+			foreach (GameObject star in stars) {
+				star.SendMessage ("Activate", SendMessageOptions.DontRequireReceiver);
+			}
+		}
 	}
 }
---
+++ Assets/Closing cinematic/scr_star.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scr_star : MonoBehaviour {
+
+	Light[] lights;
+	MeshRenderer meshRenderer;
+	float rngTimer = 9999;
+	bool activated = false;
+
+	// Use this for initialization
+	void Start () {
+		meshRenderer = GetComponent<MeshRenderer> ();
+		lights = GetComponentsInChildren<Light> ();
+
+		meshRenderer.enabled = false;
+		foreach (Light thislight in lights) {
+			thislight.enabled = false;
+		}
+	}
+
+	void Update(){
+		if (rngTimer <= 0 && activated == false) {
+			activated = true;
+
+			meshRenderer.enabled = true;
+			foreach (Light thislight in lights) {
+				thislight.enabled = true;
+			}
+		}
+
+		rngTimer -= Time.deltaTime;
+	}
+	
+	// Update is called once per frame
+	void Activate () {
+		rngTimer = Random.Range (2.4f, 5f);
+	}
+}
