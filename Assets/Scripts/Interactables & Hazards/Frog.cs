6987fed645b9da8a136db93eda84ade149c38a16
EagleEye247 <1000519@student.sae.edu.au>
2015-04-28 13:38:44 +1000

    Edited tongue script

--- Assets/Scripts/Interactables & Hazards/Frog.cs
+++ Assets/Scripts/Interactables & Hazards/Frog.cs
@@ -17,35 +17,36 @@ public class Frog : MonoBehaviour {
 	void Start () {
 		frogTongue = GetComponent<LineRenderer> ();
 		frogTongue.SetPosition (0, transform.position);
-		frogTongue.SetWidth (1f, 1f);
+		frogTongue.SetWidth (0.5f, 0.5f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	//Start counter and tongueTimer
 		counter -= Time.deltaTime;
 		tongueTimer -= Time.deltaTime;
-
+		//Resets counter and selects all fireflies
 		if (counter <= 0){
 			counter = 2f;
 			fireflys = GameObject.FindGameObjectsWithTag("FireFly");
-
+			//Finds all fireflies' distances
 			foreach (GameObject firefly in fireflys){
 				thisFireFlyDistance = Vector3.Distance (this.transform.position, firefly.transform.position);
-
+				//Finds closest firefly
 				if (thisFireFlyDistance < fireFlyDistance){
 					closestFireFly = firefly;
 					fireFlyDistance = thisFireFlyDistance;
 				}
 			}
-
+			//Attack and destroy nearest firefly
 			if (fireFlyDistance < 30f && closestFireFly != null){
 				frogTongue.SetPosition(1, closestFireFly.transform.position);
 				tongueTimer = 0.4f;
 				Destroy (closestFireFly);
+				fireFlyDistance = 99999;
 			}
 		}
-		//
+		//Resets tongue's position after countdown
 		if (tongueTimer <= 0){
 			frogTongue.SetPosition(1, transform.position);
 		}
