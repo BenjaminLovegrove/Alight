bb134836fec27be192c7d5a5c0aa21e8cd936078
whitey84 <109848@student.sae.edu.au>
2015-05-01 16:02:38 +1000

    LOs update
    LOs update

--- Assets/Scripts/Firefly management & movement/SwarmingLO.cs
+++ Assets/Scripts/Firefly management & movement/SwarmingLO.cs
@@ -55,7 +55,7 @@ public class SwarmingLO : MonoBehaviour {
 					Vector3 delta = m_Boids[j].transform.position - m_Boids[i].transform.position;
 
 					//	If it is within the range we care about
-					if (true /*delta.magnitude < nearRange*/) {
+					if (true/*delta.magnitude < nearRange*/) {
 
 						if (true /*Vector3.Dot(delta.normalized, m_Boids[i].velocity.normalized) > 0.5f*/) {
 							count++;
@@ -68,10 +68,11 @@ public class SwarmingLO : MonoBehaviour {
 
 				//	Apply average velocity to boid.
 				if (count > 0) {
-					averageVel /= count;
-					averagePos /= count;
+					averageVel /= (float)count;
+					averagePos /= (float)count;
 
-					m_Boids[i].velocity  = (m_Boids[i].velocity * 0.99f) + (averageVel * 0.01f);
+					m_Boids[i].velocity += averageVel * Time.deltaTime * 20;
+					//m_Boids[i].velocity  = (m_Boids[i].velocity * 0.99f) + (averageVel * 0.01f);
 					m_Boids[i].velocity += (averagePos-m_Boids[i].transform.position).normalized * Time.deltaTime * 20;
 				}
 				if (m_Boids[i].velocity.magnitude > speed) {
