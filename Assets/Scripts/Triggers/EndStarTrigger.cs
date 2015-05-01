9f1c18ce2d2c6295114aeecfd792cf4c478b1e64
Benjamin <Benjamin_JL@live.com>
2015-05-01 23:30:55 +1000

    Ending Trigger

---
+++ Assets/Scripts/Triggers/EndStarTrigger.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndStarTrigger : MonoBehaviour {
+
+
+	
+	// Update is called once per frame
+	void OnTriggerEnter (Collider col) {
+		if (col.gameObject.tag =="FireFly"){
+			col.SendMessage("EndingStars");
+		}	
+	}
+}
