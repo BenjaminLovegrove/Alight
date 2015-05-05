1716f9e2272acea1b34b0e73631944c714ae70db
whitey84 <dwhitehouse2@BNE-28-18-W.bne.sae>
2015-05-05 23:09:25 +1000

    LO stuff

--- Assets/Scripts/Firefly management & movement/SwarmingLO.cs
+++ Assets/Scripts/Firefly management & movement/SwarmingLO.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 public class SwarmingLO : MonoBehaviour {
 
-	public 	float 				speed 		= 10.0f;
+	public 	float 				speed 		= 1000.0f;
 	public	float				nearRange	= 10.0f;
 	public	GameObject			boidPreFab;
 
@@ -52,16 +52,18 @@ public class SwarmingLO : MonoBehaviour {
 				//	Ignore results is comparing with self
 				if (i != j) {
 					//	Get vector of distance between the two boids
-					//Vector3 delta = m_Boids[j].transform.position - m_Boids[i].transform.position;
+					Vector3 delta = m_Boids[j].transform.position - m_Boids[i].transform.position;
 
 					//	If it is within the range we care about
-					if (true/*delta.magnitude < nearRange*/) {
+					if (delta.magnitude < nearRange) {
 
-						if (true /*Vector3.Dot(delta.normalized, m_Boids[i].velocity.normalized) > 0.5f*/) {
+						if (Vector3.Dot(delta.normalized, m_Boids[i].velocity.normalized) > 0.5f)
 							count++;
 
-							averageVel += m_Boids[j].velocity;
-							averagePos += m_Boids[j].transform.position;
+						averageVel += m_Boids[j].velocity;
+						averagePos += m_Boids[j].transform.position;
+						if (delta.magnitude < 50) {
+							m_Boids[i].transform.position -= delta.normalized * Time.deltaTime * (800 / (delta.magnitude + 1));
 						}
 					}
 				}
@@ -71,15 +73,27 @@ public class SwarmingLO : MonoBehaviour {
 					averageVel /= (float)count;
 					averagePos /= (float)count;
 
-					m_Boids[i].velocity += averageVel * Time.deltaTime * 20;
-					//m_Boids[i].velocity  = (m_Boids[i].velocity * 0.99f) + (averageVel * 0.01f);
+					//m_Boids[i].velocity += averageVel * Time.deltaTime * 20;
+					m_Boids[i].velocity  = (m_Boids[i].velocity * 0.99f) + (averageVel * 0.01f);
 					m_Boids[i].velocity += (averagePos-m_Boids[i].transform.position).normalized * Time.deltaTime * 20;
 				}
 				if (m_Boids[i].velocity.magnitude > speed) {
 					m_Boids[i].velocity = m_Boids[i].velocity.normalized * speed;
 				}
 
-				m_Boids[i].GetComponent<Rigidbody>().velocity = m_Boids[i].velocity;
+				m_Boids[i].velocity += m_Boids[i].velocity * Time.deltaTime;
+
+				Rigidbody rb = m_Boids[i].GetComponent<Rigidbody>();
+				rb.position = m_Boids[i].velocity * Time.deltaTime;
+
+				if(rb.position.x<0)
+					rb.position = new Vector3(rb.position.x + 1024, rb.position.y, rb.position.z);
+				if(rb.position.x>1023)
+					rb.position = new Vector3(rb.position.x - 1024, rb.position.y, rb.position.z);
+				if(rb.position.y<0)
+					rb.position = new Vector3(rb.position.x, rb.position.y + 768, rb.position.z);
+				if(rb.position.y>767)
+					rb.position = new Vector3(rb.position.x, rb.position.y - 768, rb.position.z);
 			}
 		}
 	}
